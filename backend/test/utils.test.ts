77450c57148df0c39a48e0883de69969b9008a98
Ichigo0125 <152695829+Ichigo0125@users.noreply.github.com>
2026-04-08 11:25:55 +0000

    fix: update fabonacci tests and add 2 test cases for myCustomAdd

--- backend/test/utils.test.ts
+++ backend/test/utils.test.ts
@@ -17,20 +17,25 @@ describe('math utils testing', () => {
     it('should return 5 when add 2 and 3', () => {
       expect(myCustomAdd(2, 3)).toBe(5)
     })
+    //my test case 1
+    it('should return 1 when add -2 and 3', () => {
+      expect(myCustomAdd(-2, 3)).toBe(1)
+    })
   })
 
   describe('fabonacci testing', () => {
     it('should return 1 when n is 1', () => {
-     // TODO: fix the test
-      expect.fail('not implemented')
+      expect(fabonacci(1)).toBe(1)
     })
     it('should return 1 when n is 2', () => {
-      // TODO: fix the test
-      expect.fail('not implemented')
+      expect(fabonacci(2)).toBe(1)
     })
     it('should return 2 when n is 3', () => {
-      // TODO: fix the test
-      expect.fail('not implemented')
+      expect(fabonacci(3)).toBe(2)
+    })
+    //my test case 2
+    it('should return 3 when n is 4', () => {
+      expect(fabonacci(4)).toBe(3)
     })
   })
 })
